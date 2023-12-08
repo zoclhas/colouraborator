a1ef79bd76630bcec0264c2bfcf561871e817cc2
Zoclhas <zoclhas@gmail.com>
2023-12-08 19:17:30 +0400

    login page further stuff

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,7 +1,7 @@
 import { LoginForm } from "./form";
 import { Card, CardContent } from "@/components/ui/card";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
-import { AlertCircle } from "lucide-react";
+import { AlertCircle, CheckCircle2 } from "lucide-react";
 
 import type { Metadata } from "next";
 import Link from "next/link";
@@ -13,17 +13,18 @@ export const metadata: Metadata = {
 export default function Login({
   searchParams,
 }: {
-  searchParams: { message?: string };
+  searchParams: { message?: string; success?: string };
 }) {
   const message = searchParams.message;
+  const success = searchParams.success;
 
   return (
     <main className="min-h-[calc(100vh-80px)] px-4 flex items-center justify-center">
       <div className="flex flex-col gap-2">
         {message && (
           <Alert className="sm:min-w-[370px] mb-2">
-            <AlertCircle />
-            <AlertTitle>Uh Oh!</AlertTitle>
+            {success === "true" ? <CheckCircle2 /> : <AlertCircle />}
+            <AlertTitle>{success === "true" ? "Ta-da!" : "Uh Oh!"}</AlertTitle>
             <AlertDescription>{message}</AlertDescription>
           </Alert>
         )}
@@ -33,13 +34,16 @@ export default function Login({
         </Card>
 
         <Card>
-          <CardContent className="flex justify-between gap-2 pb-0 p-2">
+          <CardContent className="flex justify-between gap-2 pb-0 p-2 text-sm">
             <Link href="/login/forgot" className="underline">
               Forgot Password?
             </Link>
-            <Link href="/register" className="underline">
-              Register
-            </Link>
+            <span>
+              New Here?&nbsp;
+              <Link href="/register" className="underline">
+                Register
+              </Link>
+            </span>
           </CardContent>
         </Card>
       </div>
